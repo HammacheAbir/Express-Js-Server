8c7983d505145fa9649bb771972e93cdd9f63d5c
Abir <abirhammache@gmail.com>
2020-04-07 02:37:52 -0700

    add node_modules to gitignore

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,7 +2,7 @@ const moment =require('moment');
 
 const logger=(req,res,next)=>{
     console.log(`${req.protocol}//${req.get('host')}${req.originalUrl}: ${moment().format()}`)
-    req.protocole
+    //req.protocole
     next()
 }
 
